0d3b6ad0c8516df5b5e7dff4b58e4d159920f4d4
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-05-27 08:54:02 +0300

    Done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,39 @@
-#!/usr/bin/env python3
+
+import re
 
 class MyString:
-  pass
+    def __init__(self):
+        self._value = ""
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            raise ValueError("Value must be a string")
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        sentences = re.split(r'[.!?]', self._value)
+        return len([sentence for sentence in sentences if sentence.strip() != ""])
+
+string = MyString()
+string.value = "This is a string! It has three sentences. Right?"
+
+print(string.is_sentence())  # True
+print(string.is_question())  # False
+print(string.is_exclamation())  # False
+print(string.count_sentences())  # 3
+
